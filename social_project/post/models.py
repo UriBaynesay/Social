4692fbbf0fddbfad0ce0ef3bbe3dfa4b2b1e5ebf
Uri Baynesay <uri552@gmail.com>
2024-06-30 12:07:49 +0300

    added compose form

--- social_project/post/models.py
+++ social_project/post/models.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -7,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
+    content = models.CharField(max_length=250)
     date_created = models.DateField(default=timezone.now)
 
     objects = models.Manager()
