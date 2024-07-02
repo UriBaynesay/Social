21bf67763308a724a83a9ada937d0f4dcd6bdb87
Uri Baynesay <uri552@gmail.com>
2024-07-02 12:23:04 +0300

    added login and register

--- social_project/social_project/urls.py
+++ social_project/social_project/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path, include
 from post import urls as post_urls
+from users.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(post_urls))
+    path('', include(post_urls)),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('register/', register, name='register'),
 ]
