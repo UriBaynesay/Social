4ad8377119ae73f2deed66c30380493db77c5c35
Uri Baynesay <uri552@gmail.com>
2024-07-08 09:59:10 +0300

    added delete post

--- social_project/post/urls.py
+++ social_project/post/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, post_details, post_create, post_update
+from .views import home, post_details, post_create, post_update, post_delete
 
 urlpatterns = [
     path('', home, name='home'),
     path('post/<int:pk>/', post_details, name='post-details'),
     path('post/update/<int:pk>/', post_update, name='post-update'),
-    path('post/new/', post_create, name='post-create')
+    path('post/new/', post_create, name='post-create'),
+    path('post/delete/<int:pk>', post_delete, name='post-delete')
 ]
